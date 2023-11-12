a6718166df6d1de8166e7625f19f93ea1f9014dc
FilipZapendowski <148978059+FilipZapendowski@users.noreply.github.com>
2023-11-12 19:04:02 +0100

    Update ResourceManager.hpp

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,37 @@
 #pragma once
 
-#include "Resource.hpp"
 
-class ResourceManager
-{
-    // Twoja implementacja tutaj
+#include "Resource.hpp"
+#include <memory>
+
+class ResourceManager {
+public:
+    ResourceManager() : resource(std::make_shared<Resource>()) {} 
+
+    double get() {
+        if (resource) {
+            return resource->get(); 
+        } else {
+            throw std::runtime_error("Resource not initialized");
+        }
+    }
+
+    ResourceManager(const ResourceManager& other) : resource(other.resource) {}
+    ResourceManager(ResourceManager&& other) noexcept : resource(std::move(other.resource)) {}
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            resource = other.resource;
+        }
+        return *this;
+    }
+    ResourceManager& operator=(ResourceManager&& other) noexcept {
+        if (this != &other) {
+            resource = std::move(other.resource);
+        }
+        return *this;
+    }
+    ~ResourceManager() {}
+
+private:
+    std::shared_ptr<Resource> resource; // Test wskaźnika inteligentnego
 };
